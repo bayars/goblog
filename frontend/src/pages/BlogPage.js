7ae98e096e0746f639364b6e1cb2d186dd7cedd5
Safa Bayar <safa@dev.safabayar.com>
2024-01-13 15:46:55 -0500

    new components

---
+++ frontend/src/pages/BlogPage.js
@@ -0,0 +1,43 @@
+import * as React from "react";
+
+function BlogPage() {
+  return (
+    <div className="blog-page">
+        <span className="span-2">BLOG</span>
+        <span className="span-3">
+            <div className="div-8">
+            27
+            <br />
+            MAY
+            <br />
+            </div>
+            <span className="span-4">
+            <div className="div-9">
+                15 Disadvantages Of Freedom And How You Can Workaround It.
+            </div>
+            <div className="div-10">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum{" "}
+                {/* <span style="font-weight: 200; color: rgba(59,64,187,1);">
+                ...read more
+                </span> */}
+            </div>
+            <span className="span-5">#tag</span>
+            </span>
+        </span>
+    </div>
+  );
+}
+
+export default BlogPage;
+
+
+
+
+
+
---
+++ frontend/src/pages/Links.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Links() {
+    return (
+      <div className="links">
+        <div className="overlap-group-wrapper">
+          <div className="overlap-group">
+            <div className="text-box">
+              GITHUB:
+              <br />
+              GITLAB
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  export default Links;
